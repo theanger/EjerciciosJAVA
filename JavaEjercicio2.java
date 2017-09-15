354048831042431bb4021ea27ec6d625c976b41c
theanger <31898638+theanger@users.noreply.github.com>
2017-09-15 13:42:19 -0300

    Ejercicio 2 de Java
    Completado.

---
+++ JavaEjercicio2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaejercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class JavaEjercicio2 {
+
+    /**Ejercicio 2: Se debe pedir el nombre y luego mostrarlo
+     *
+     */
+    public static void main(String[] args) {
+    
+    String nomb;    
+    Scanner mis = new Scanner(System.in);
+    System.out.println("Ingrese su nombre:");
+    nomb= mis.nextLine();
+    System.out.println("Su nombre es:" + nomb);
+    
+            
+// TODO code application logic here
+    }
+    
+}
