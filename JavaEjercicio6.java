806bf0554f6277fdd378f2f77bf4c81ec5aa2ec8
theanger <31898638+theanger@users.noreply.github.com>
2017-09-18 13:23:44 -0300

    Ejercicio 6
    Completado

---
+++ JavaEjercicio6.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaejercicio6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class JavaEjercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nota1 ,nota2 ,nota3 ;
+        int resultado; 
+        Scanner msc= new Scanner(System.in);
+        System.out.println("Ingrese primera nota: ");
+        nota1= msc.nextInt();
+        System.out.println("Ingrese segunda nota: ");
+        nota2= msc.nextInt();
+        System.out.println("Ingrese tercer nota: ");
+        nota3= msc.nextInt();
+        resultado = (nota1+nota2+nota3) /3;
+        System.out.println("El promedio total es: " + resultado);
+        
+        
+        // TODO code application logic here
+    }
+    
+}
