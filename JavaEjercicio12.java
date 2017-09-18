8d487c4643697edb0327013597421b671e2c46b2
theanger <31898638+theanger@users.noreply.github.com>
2017-09-18 13:49:04 -0300

    Ejercicio 12
     Completo

---
+++ JavaEjercicio12.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaejercicio12;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class JavaEjercicio12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int num;
+     int porcentaje;
+     double resultado;
+     
+     Scanner msc= new Scanner(System.in);
+     System.out.println("Ingrese el importe: ");
+     num= msc.nextInt();
+     System.out.println("Ingrese el porcenaje de impuesto: ");
+     porcentaje= msc.nextInt();
+     resultado= num * porcentaje /100 ;
+     System.out.println("El agregado de impuestos es: " + resultado);
+     
+     
+     
+        
+     // TODO code application logic here
+    }
+    
+}
