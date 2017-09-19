b2e22f968e9dd513bd148a75dc7d7a299538784e
theanger <31898638+theanger@users.noreply.github.com>
2017-09-19 17:59:32 -0300

    completo, cambio

--- JavaEjercicio14.java
+++ JavaEjercicio14.java
@@ -15,7 +15,10 @@ import java.util.Scanner;
 public class JavaEjercicio14 {
 
     /**
-     * @param args the command line arguments
+     * 15, se ingresa un importe, si supera los $100 se le suma un 23%, si es menor a 100
+     * se le descuenta el 50%
+     * 16, Se ingresa el importe de un pasaje de avión y el mes de viaje, si es enero; se les descuenta un 10%
+     * por temporada baja.
      */
     public static void main(String[] args) {
      
